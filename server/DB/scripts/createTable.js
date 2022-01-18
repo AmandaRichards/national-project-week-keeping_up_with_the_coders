5841d392ab17a844b0da10fde2c322dfe150833f
patrick <tasniim_bari@yahoo.com>
2022-01-18 11:42:01 +0000

    created tables on heroku

--- server/DB/scripts/createTable.js
+++ server/DB/scripts/createTable.js
@@ -1,13 +1,13 @@
 import query from "../index.js";
 
 const dataResources =
-  "CREATE TABLE IF NOT EXISTS resources(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT) RETURNING *;";
+  "CREATE TABLE IF NOT EXISTS resources(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT);";
 
 const dataVideos =
-  "CREATE TABLE IF NOT EXISTS videos(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT) RETURNING *;";
+  "CREATE TABLE IF NOT EXISTS videos(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT);";
 
 const dataLinks =
-  "CREATE TABLE IF NOT EXISTS links(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT) RETURNING *;";
+  "CREATE TABLE IF NOT EXISTS links(id SERIAL PRIMARY KEY, Week INT, Type_of_resource TEXT);";
 
 async function createResources() {
   const res = await query(dataResources);
