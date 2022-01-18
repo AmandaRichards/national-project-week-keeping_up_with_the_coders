96c1f2812f1315442b02536e51dd65e4a41c2916
Israel <93683580+blueishegg@users.noreply.github.com>
2022-01-18 11:31:59 +0000

    creating tables

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,10 +1,16 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
- user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    password: process.env.PASSWORD,
-   ssl: { rejectUnauthorized: false}
-})
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  password: process.env.PASSWORD,
+  ssl: { rejectUnauthorized: false },
+});
+
+function query(text, params) {
+  return pool.query(text, params);
+}
+
+export default query;
