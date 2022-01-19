35ffff2a00b1857aea33fd663c7966720b08f02a
Anita Caira Percival <anita.percival1@gmail.com>
2022-01-19 14:43:55 +0000

    Delete by id and delete all

--- server/routes/resourcesResources/routesResources.js
+++ server/routes/resourcesResources/routesResources.js
@@ -3,9 +3,35 @@ import express from "express";
 import * as functionality from "../resourcesResources/resourcesModule.js";
 const routerResources = express.Router();
 
+/* GET home page */
 routerResources.get("/", async function (req, res) {
   const responseResources = await functionality.getAllResources();
   return res.json({ success: true, payload: { responseResources } });
 });
-
 export default routerResources;
+
+// post resources
+routerResources.post("/", async function (req, res) {
+  const body = req.body;
+  console.log(body);
+  const responseResources = await functionality.postResources(body);
+  return res.json({ success: true, payload: { responseResources } });
+});
+
+
+// delete resource by id
+routerResources.delete("/:id", async function (req, res) {
+  let id = Number(req.params.id);
+  const data = await functionality.deleteResourcesByID(id);
+  return res.json({ success: true, payload: { data } })
+})
+
+// delete all resources
+
+routerResources.delete("/", async function (req, res) {
+  const data = await functionality.deleteAllResources();
+  return res.json({success:true,payload:{data}})
+
+});
+
+
