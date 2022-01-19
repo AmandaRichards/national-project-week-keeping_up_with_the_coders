1fd4e00f98fafb582dac200d750026496f715351
AmandaRichards <Richards.amanda92@gmail.com>
2022-01-19 16:09:59 +0000

    pushing israel

---
+++ src/Components/HomePage/Thumbnail/index.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Thumbnail({ title, imageUrl }) {
+  return (
+    <div>
+      <img src={imageUrl} alt="alt" />
+      npm 
+    </div>
+  );
+}
+
+export default Thumbnail;
