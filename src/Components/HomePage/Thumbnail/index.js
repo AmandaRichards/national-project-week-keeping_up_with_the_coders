5923072493e41ba8345e33425e764f265689adef
AmandaRichards <Richards.amanda92@gmail.com>
2022-01-19 14:21:53 +0000

    links work again

--- src/Components/HomePage/Thumbnail/index.js
+++ src/Components/HomePage/Thumbnail/index.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-function Thumbnail({ title, imageUrl, link }) {
+function Thumbnail({ title, imageUrl }) {
   return (
     <div>
       <img src={imageUrl} alt="alt" />
-      <Link to={link}>{title}</Link>
+      <Link to="/week1">{title}</Link>
     </div>
   );
 }
