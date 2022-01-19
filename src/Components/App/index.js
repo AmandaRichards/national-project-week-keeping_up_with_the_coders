04b4dcd83803e444dbdaa8963400bd2aa186230f
patrick <tasniim_bari@yahoo.com>
2022-01-19 14:39:42 +0000

    css react content-page set up

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "../HomePage/index";
 import Login from "../Login/index.js";
+import Content from "../contentPage/index.js"
 
 import Week1 from "./ContentPages/Week1";
 
@@ -22,10 +23,7 @@ function App() {
         <Route path="homepage" element={<HomePage />} />
         <Route path="week1" element={<Week1 />} />
       </Routes>
-
-
     <Content/>
-
 
     </div>
   );
