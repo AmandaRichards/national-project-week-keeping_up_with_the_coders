ed0792e4b9a64f2fb3557c6ae626b9660ea62077
Anita Caira Percival <anita.percival1@gmail.com>
2022-01-20 12:03:43 +0000

    Tile images

--- src/Components/HomePage/DummyData/DummyData.js
+++ src/Components/HomePage/DummyData/DummyData.js
@@ -2,28 +2,42 @@ const dummydata = [
   {
     id: 1,
     title: "Week 1: HTML and CSS",
-    imageUrl: "https://miro.medium.com/max/2688/1*Q8w9PI58DKjolhl5aDeiOQ.png",
+    imageUrl: "https://cdn-icons.flaticon.com/png/512/2085/premium/2085206.png?token=exp=1642679944~hmac=21d8e5b051e6dd084a6c79ac2b001817",
     // link: "/week1",
   },
   {
     id: 2,
     title: "Week 2: JavaScript",
     imageUrl:
-      "https://www.tutorialrepublic.com/lib/images/javascript-illustration.png",
+      "https://cdn-icons.flaticon.com/png/512/3097/premium/3097978.png?token=exp=1642680111~hmac=4d9faf5bacc803191d2301dc10b07e1f",
     // link: "/week1",
   },
   {
     id: 3,
     title: "Week 3: UX and UI",
     imageUrl:
-      "https://www.datocms-assets.com/14946/1633281680-ux-vs-ui-cover-edited.png?auto=format&w=912",
+      "https://cdn-icons.flaticon.com/png/512/1367/premium/1367672.png?token=exp=1642680229~hmac=2de6f90fb20b71a49fa3d1b8ad150f2a",
     // link: "/week1",
   },
   {
     id: 4,
     title: "Week 4: Backend",
     imageUrl:
-      "https://alterrasoft.com/wp-content/uploads/2019/05/backend-for-article-2.jpg",
+      "https://cdn-icons-png.flaticon.com/512/1073/1073436.png",
+    // link: "/week1",
+  },
+  {
+    id: 5,
+    title: "Week 5: Databases",
+    imageUrl:
+      "https://cdn-icons-png.flaticon.com/512/2906/2906274.png",
+    // link: "/week1",
+  },
+  {
+    id: 6,
+    title: "Week 6: Testing",
+    imageUrl:
+      "https://cdn-icons-png.flaticon.com/512/2942/2942789.png",
     // link: "/week1",
   },
 ];
--- src/Components/HomePage/Thumbnail/index.js
+++ src/Components/HomePage/Thumbnail/index.js
@@ -1,10 +1,11 @@
-// import "./index.css";
+import "./index.css";
 import { Link } from "react-router-dom";
 
 function Thumbnail({ title, imageUrl }) {
   return (
     <div>
-      <img src={imageUrl} alt="alt" />
+      <img src={imageUrl} alt="alt" className="tile" />
+      <br></br>
       <Link to="/contentpage">{title}</Link>
     </div>
   );
