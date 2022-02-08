d706da898b25626fa57d10e8c6d8e0c83a33fc6f
patrick <tasniim_bari@yahoo.com>
2022-02-08 19:21:08 +0000

    authzeroadminrolelogin

--- src/Components/HomePage/Heading/index.js
+++ src/Components/HomePage/Heading/index.js
@@ -5,13 +5,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 function Heading() {
   // const what = username;
   const { logout, user, isAuthenticated } = useAuth0();
-
+  // console.log(user["https://example.com/roles"][0])
   // const [name,setName]=useState(username)
   return (
     isAuthenticated && (
       <div className="header-homepage-page">
         <div className="header-title-homepage">
-          <p>Welcome, {user.name}</p>
+          <p>Welcome, {user.name} {user["https://example.com/roles"][0]}</p>
         </div>
         <div className="header-logout-homepage">
           <button onClick={() => logout({ returnTo: window.location.origin })}>
--- src/Components/HomePage/Thumbnail/index.js
+++ src/Components/HomePage/Thumbnail/index.js
@@ -1,8 +1,12 @@
 import "./index.css";
 import { Link } from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
 
-function Thumbnail({ title, imageUrl,week ,password, username}) {
+function Thumbnail({ title, imageUrl,week ,password}) {
+  const { user, isAuthenticated } = useAuth0();
+  
   return (
+     isAuthenticated && (
     <div className="mainContainer">
       <div className="work">
       <div className="img-container">
@@ -10,23 +14,15 @@ function Thumbnail({ title, imageUrl,week ,password, username}) {
       </div>
       <br></br>
       
-      {/* <Link to="/contentpage" className="linkText">
-        {title}
-      </Link> */}
-     
+  
       <div className="link-container">
-        {/* { if(password === "admin"){
-        <Link to="/contentpage" className="linkText" state={{ title: {title},week:{week} }}>
-          {title}
-        </Link>
-        }
-        } */}
-        {password === "admin" ? <Link to="/contentpage" className="linkText" state={{ title: {title},week:{week} }}><div>{title}</div></Link>: <Link to="/contentpageuser" className="linkText" state={{ title: {title},week:{week} }}>
+        {user["https://example.com/roles"][0] === "admin" ? <Link to="/contentpage" className="linkText" state={{ title: {title},week:{week} }}><div>{title}</div></Link>: <Link to="/contentpageuser" className="linkText" state={{ title: {title},week:{week} }}>
             <div>{title}</div>
         </Link>}
       </div>
       </div>
     </div>
+     )
   );
 }
 export default Thumbnail;
